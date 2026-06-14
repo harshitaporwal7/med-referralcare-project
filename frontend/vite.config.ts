78fc9019d05df3a92e1bfb52ad21014eeccd8df8
harshita porwal <harshitaporwal7@email.com>
2026-06-14 16:21:05 +0530

    fix configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://med-referralcare-project.onrender.com',
         changeOrigin: true,
       },
     },
