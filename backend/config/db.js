bc4d233b816170386d1d820e9be8086d68cf9411
harshitaporwal7 <harshitaporwal7@gmail.com>
2026-07-01 20:46:33 +0530

    Update database connection configuration variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'med_referral',
-
-  ssl: {
-    rejectUnauthorized: false
-  },
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  port: process.env.MYSQLPORT || process.env.DB_PORT,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || 'med_referral',
 
   waitForConnections: true,
   connectionLimit: 10,
